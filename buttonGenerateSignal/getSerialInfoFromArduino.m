65dfe8d0d6adfa18f16c7d947927b5496b7bb767
Lilyn Gao <lilyn.gao@altaerosenergies.com>
2017-03-25 18:45:48 -0400

    matlab rev

--- buttonGenerateSignal/getSerialInfoFromArduino.m
+++ buttonGenerateSignal/getSerialInfoFromArduino.m
@@ -4,19 +4,26 @@
 % Function to read sensor output from arduino
 
 %clear worksapce
-clear a
+clear 
+clc
+
+sec = 10;
+
+a = arduino('COM4','uno');
 % Define a rudiment
 % Define a duration of practice
-a = arduino('COM4','uno');
-interv = 1000;
-passo = 1;
-t = 1;
+% estimate 30 points per second
+timeDuration = seconds(sec);
+interv = sec*30;
+
+timeNow = now;
+timeEnd = timeNow + timeDuration;
+
 x = 0;
 
-%define ideal rudement
-
 %setup while loop for real time plotting
-while t<interv
+
+while now<timeEnd
     %set b as current point being read
     b = a.readDigitalPin(2);
     %plot it with previously acquired data
@@ -24,9 +31,8 @@ while t<interv
     plot(x)
     axis([0,interv,0,1.5]);
     grid on
-    %increase time step and draw
-    t = t+passo;
     drawnow;
 end
+
 
 
