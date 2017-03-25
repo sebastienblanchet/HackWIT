ee6a7c1e89ce6e8e4c8a19b026d717857cc0ecb9
Sebastien Blanchet <s3blanch@edu.uwaterloo.ca>
2017-03-25 18:43:15 -0400

    VIbration sensor test

---
+++ VibeSensor/VibeSensor.ino
@@ -0,0 +1,35 @@
+/* HackWITus
+    sebastien blanchet
+    March 25 2017
+    https://tkkrlab.nl/wiki/Arduino_KY-002_Vibration_switch_module namne of sernsor
+*/
+
+int ledPin = 13;
+int ShockVal = 10;
+int val;
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ledPin, OUTPUT);
+  pinMode(ShockVal, INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  val = digitalRead(ShockVal);
+  Serial.print("The sensor is ");
+  Serial.println(val);
+  
+  if (val == HIGH)
+  {
+    digitalWrite(ledPin, LOW);
+
+  }
+  else
+  {
+    digitalWrite(ledPin, HIGH);
+  }
+  delay(1000);
+}
